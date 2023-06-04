61924548e8d9af4674443f2f3ca873ba152c1446
Rian Neris <86022706+RianNeris@users.noreply.github.com>
2023-06-03 23:51:40 -0300

    Update medidaController.js

--- src/controllers/medidaController.js
+++ src/controllers/medidaController.js
@@ -107,11 +107,26 @@ function gerarDadosRandom(req, res) {
     });
 }
 
+function receberTemperaturaContainers(req, res) {
+    medidaModel.receberTemperaturaContainers().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
     atualizarGraficoDashboard,
     receberTemperaturaLotes,
     receberContainers,
-    gerarDadosRandom
+    gerarDadosRandom,
+    receberTemperaturaContainers
 }
